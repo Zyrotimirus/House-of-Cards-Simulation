224279197cc8a4ef9724a4796d487cbaff0b6627
Zitrone <mueller.sven-@outlook.de>
2018-06-08 22:24:42 +0200

    card-creation added (space), card rotation added (A, D), creation and edit Mode switch added (leftShift)

---
+++ Assets/Scripts/CardManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardManager : MonoBehaviour {
+
+    public GameObject card;
+    public GameObject preparedCard;
+    public GameObject cardsParent;
+    private List<Transform> cards;
+    private Vector3 mousePos;
+
+    public bool creationMode = true;
+
+	void Start () {
+        cards = new List<Transform>();
+        CreateCard(preparedCard);
+	}
+	
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.LeftShift)) {
+            creationMode = !creationMode;
+        }
+        if(creationMode) {
+            CreationMode();
+        } else {
+            EditMode(); 
+        }
+        cards[0].gameObject.SetActive(creationMode);
+    }
+
+    public void CreationMode()
+    {
+        mousePos = Input.mousePosition;
+        mousePos.z = 8.0f;
+        Vector3 objectPos = Camera.main.ScreenToWorldPoint(mousePos);
+        cards[0].position = new Vector3(objectPos.x, objectPos.y, objectPos.z);
+
+        RotateCard();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CreateCard(card);
+        }
+    }
+
+    public void EditMode()
+    {
+        
+    }
+
+    public void CreateCard(GameObject obj)
+    {
+        float angle = 0;
+        if (obj.name == "Card") {
+            angle = cards[0].eulerAngles.z;
+        }
+        Vector3 objectPos = Camera.main.ScreenToWorldPoint(mousePos);
+        var clone = Instantiate(obj, objectPos, Quaternion.Euler(0, 0, angle), cardsParent.transform);
+        cards.Add((Transform)clone.transform);
+    }
+
+    public void RotateCard()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            cards[0].eulerAngles += new Vector3(0, 0, 2f);
+        } else if(Input.GetKey(KeyCode.D))
+        {
+            cards[0].eulerAngles += new Vector3(0, 0, -2f);
+        }
+    }
+
+}
