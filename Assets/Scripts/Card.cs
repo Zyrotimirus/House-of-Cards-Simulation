365bd0ba383bb9099e80c1a117751122e6b49d78
malatoma <marcowieditz@gmail.com>
2018-06-22 21:41:18 +0200

    provisorischer impact

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class Card : MonoBehaviour {
 
+    public Rigidbody rb;
+
 	// Use this for initialization
 	void Start () {
-		
+        rb = gameObject.GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+    private void OnCollisionEnter(Collision collision)
+    {
+        rb.velocity = collision.rigidbody.velocity * -1;
+    }
 }
