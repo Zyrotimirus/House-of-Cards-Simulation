be030443cb2e60a2e2e080fff8214e6f5853d757
Dimos <dontmindme12@web.de>
2018-06-24 14:58:30 +0200

    Added Textures and fixed multi-row placement again

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -4,20 +4,28 @@ using UnityEngine;
 
 public class Card : MonoBehaviour {
 
-    public Rigidbody rb;
+    //public Rigidbody rb;
 
+    Renderer m_Renderer;
 	// Use this for initialization
 	void Start () {
-        rb = gameObject.GetComponent<Rigidbody>();
-	}
+        //rb = gameObject.GetComponent<Rigidbody>();
+        
+        m_Renderer = GetComponent<Renderer>();
+        Object[] textures = Resources.LoadAll("Textures/card_textures", typeof(Texture2D));
+        Texture2D texture = (Texture2D)textures[Random.Range(0, textures.Length)];
+        m_Renderer.material.mainTexture = texture;
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        rb.velocity = collision.rigidbody.velocity * -1;
-    }
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    rb.velocity = collision.rigidbody.velocity * -1;
+    //}
+
+
 }
