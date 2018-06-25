37659d3779992468999dfde50275e83125ac1a61
Unknown <dontmindme12@web.de>
2018-06-25 17:20:25 +0200

    Add dynamic friction v1 to cards

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class Card : MonoBehaviour {
 
     public int id;
+    public float minimumCardFrictionMoving;
+    public float maximumCardFrictionMoving;
+
     private Rigidbody rb;
+    private float oldVelo;
+    private float curVeloDifference;
 
     Renderer m_Renderer;
 	
@@ -16,7 +21,29 @@ public class Card : MonoBehaviour {
         Object[] textures = Resources.LoadAll("Textures/card_textures", typeof(Texture2D));
         Texture2D texture = (Texture2D)textures[Random.Range(0, textures.Length)];
         m_Renderer.material.mainTexture = texture;
+        oldVelo = rb.velocity.sqrMagnitude;
     }
+
+    void update()
+    {
+        curVeloDifference = oldVelo - rb.velocity.sqrMagnitude;
+        if (curVeloDifference <= 0.1f)
+        {
+            if (GetComponent<Collider>().material.dynamicFriction > minimumCardFrictionMoving)
+            {
+                GetComponent<Collider>().material.dynamicFriction -= 0.01f;
+            }
+        }
+        else if (curVeloDifference >= 0.1f)
+        {
+            if (GetComponent<Collider>().material.dynamicFriction < maximumCardFrictionMoving)
+            {
+                GetComponent<Collider>().material.dynamicFriction += 0.01f;
+            }
+        }
+    }
+
+
 
     private void OnCollisionEnter(Collision collision)
     {
